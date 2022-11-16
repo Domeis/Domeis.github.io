e76431a4774d649f0ef433fdab3ae1a7c6cbd8a3
Whani Kim <46833402+Domeis@users.noreply.github.com>
2022-11-17 04:18:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const navSlide = () => {
     nav.classList.toggle("nav-active");
 
     //Animate Links
-    navLinks.forEach(link, index) => {
+    navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
         link.style.animation =
-          "navLinkFade 0.5s ease forwards  ${index / 7 + 0.3}s";
+          "navLinkFade 0.5s ease forwards  ${index / 4 + 0.3}s";
       }
     });
     //burger animation
